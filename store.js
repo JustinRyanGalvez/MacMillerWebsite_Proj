a67dec44f02d7f3ee899c815633e11e10eb5fc9e
Justin G <galvezjustin@hotmail.com>
2022-06-17 14:01:06 -0700

    Started JS, remove button and clicks are functional

--- store.js
+++ store.js
@@ -1,1 +1,21 @@
-console.log("Here")
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('class-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quality-input')[0]
+        console.log(priceElement, quantityElement)
+    }
+}
